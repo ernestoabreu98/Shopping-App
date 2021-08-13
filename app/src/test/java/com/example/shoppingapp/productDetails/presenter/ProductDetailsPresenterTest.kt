8b07e98f80bda3a36fa42f099654a8629b0989be
Ernesto Abreu <ernesto.abreu@globant.com>
2021-08-13 13:04:10 -0300

    Test the onFavoriteImageViewPressed callback on Presenter

--- app/src/test/java/com/example/shoppingapp/productDetails/presenter/ProductDetailsPresenterTest.kt
+++ app/src/test/java/com/example/shoppingapp/productDetails/presenter/ProductDetailsPresenterTest.kt
@@ -12,6 +12,8 @@ import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.KArgumentCaptor
+import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.whenever
 
 @ExperimentalCoroutinesApi
@@ -55,6 +57,23 @@ class ProductDetailsPresenterTest {
 
             verify(view).setProductDetailsContent()
             verify(view).hideProductAsFavorite()
+        }
+    }
+
+    @Test
+    fun `Check if presenter is responding when the onImageButtonPressed is being called on the View`() {
+        val product: Products = Products(3, "Pants", 12.00, "Pants for man", "Clothes", "http://")
+        runBlocking {
+            whenever(view.getProductSelected()).thenReturn(product.id)
+            whenever(model.checkIsFavorite(view.getProductSelected())).thenReturn(false)
+
+            ProductDetailsPresenter(view, model)
+
+            val argumentCaptor: KArgumentCaptor<(Products) -> Unit> = argumentCaptor()
+            verify(view).onFavoriteImageViewPressed(argumentCaptor.capture())
+            argumentCaptor.firstValue.invoke(product)
+            verify(model).saveFavorite(product)
+            verify(view).showProductAsFavorite()
         }
     }
 }
